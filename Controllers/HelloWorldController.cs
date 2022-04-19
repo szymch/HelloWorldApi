e84bb084be01cb81ede1fc7c149c7403c2a5551f
Szymon Chojnacki <13176163+szymch@users.noreply.github.com>
2022-04-19 15:13:37 +0200

    Endpoints to display build date and current time

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,9 +1,9 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloWorldApi.Controllers;
 
 [ApiController]
-[Route("controller")]
 public class HelloWorldController : ControllerBase
 {
     private readonly ILogger<HelloWorldController> _logger;
@@ -18,5 +18,24 @@ public class HelloWorldController : ControllerBase
     public string HelloWorld()
     {
         return "Hello from REST API";
+    }
+
+    [HttpGet]
+    [Route("BuildDate")]
+    public string BuildDate()
+    {
+        var version = Assembly.GetExecutingAssembly().FullName.Split(',')[1].Trim();
+        int buildNumber = Convert.ToInt32(version.Split('.')[2]);
+            int revision = Convert.ToInt32(version.Split('.')[3]);
+        DateTime buildDate = new DateTime(2000, 1, 1)
+            .Add(new TimeSpan(buildNumber, 0, 0, 2 * revision, 0));
+        return $"Build date: {buildDate:yyyy-MM-dd HH:mm:ss}";
+    }
+
+    [HttpGet]
+    [Route("")]
+    public string Index()
+    {
+        return "Hello World API. Current time: " + DateTime.Now.ToString("HH:mm:ss");
     }
 }
